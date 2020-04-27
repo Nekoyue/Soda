960de741fae4e5d25b288b6f409469df5827a55b
Nekoyue <cirno.yue@gmail.com>
2020-04-27 17:23:18 +0800

    Upgrade dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,18 +17,18 @@ dependencies {
     implementation(kotlin("stdlib-js"))
 
     //React, React DOM + Wrappers
-    implementation("org.jetbrains:kotlin-react:16.13.0-pre.94-kotlin-1.3.70")
-    implementation("org.jetbrains:kotlin-react-dom:16.13.0-pre.94-kotlin-1.3.70")
+    implementation("org.jetbrains:kotlin-react:16.13.1-pre.103-kotlin-1.3.72")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.103-kotlin-1.3.72")
     implementation(npm("react", "16.13.1"))
     implementation(npm("react-dom", "16.13.1"))
 
     //Kotlin Styled
-    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.94-kotlin-1.3.70")
+    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.103-kotlin-1.3.72")
     implementation(npm("styled-components"))
     implementation(npm("inline-style-prefixer"))
 
     //React Router
-    implementation("org.jetbrains:kotlin-react-router-dom:4.3.1-pre.94-kotlin-1.3.70")
+    implementation("org.jetbrains:kotlin-react-router-dom:5.1.2-pre.103-kotlin-1.3.72")
     implementation(npm("react-router-dom"))
 
     //Components
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,11 @@
 import react.dom.render
-import styled.StyledComponents
 import styled.css
 import styled.injectGlobal
 import styled.styledDiv
 import kotlin.browser.document
 
 fun main() {
-    StyledComponents.injectGlobal(GlobalStyles)
+    injectGlobal(GlobalStyles)
 
     render(document.getElementById("root")) {
         styledDiv {
